119a50c063469bc340810db5653b8d3d1f72c6ef
dinesh <dinesh@codingmart.com>
2022-05-22 12:25:22 +0530

    assign fix

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export const initConfig = () => {
-	const host = (process.env.NODE_ENV = "development"
-		? "http://localhost:8080"
-		: "https://auth-two-backend.herokuapp.com");
+	console.log(process.env.NODE_ENV);
+	const host =
+		process.env.NODE_ENV === "development"
+			? "http://localhost:8080"
+			: "https://auth-two-backend.herokuapp.com";
 	axios.defaults.baseURL = `${host}/api/v1`;
 
 	axios.defaults.headers.post["Content-Type"] = "application/json";
